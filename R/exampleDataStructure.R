0a4a6de306332fb1135319a50fa58f1fa7ced98b
Gang Chen <chengangcs@gmail.com>
2013-04-18 18:01:26 +0800

    refine dir structure, add Rd document for user-level objects

--- R/exampleDataStructure.R
+++ R/exampleDataStructure.R
@@ -1,17 +1,17 @@
-example.data.frame <- function(){
+exampleDataFrame <- function(){
   df = data.frame(names = c("Gang Chen", "Chen Gang", "Xi Dada"), scores = c(90, 93, 99), weights=c(100, 105, 70))
   print(df)
   df
 }
 
-example.list <- function(){
+exampleList <- function(){
   ll = list(a = 1, b = c("A", "B"), c = list(x="Test", y="result"), d = matrix(rnorm(16), 4, 4))
   print(ll)
   ll
 }
 
 
-example.vector <- function(){
+exampleVector <- function(){
 
   c = c(1,2,3,4,5,6,8)
   print(c)
